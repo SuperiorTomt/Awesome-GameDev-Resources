44acdadb04a22722683bc5fe8fc2e18c5bed8de8
SuperiorTomt <thomas.smith03@mymail.champlain.edu>
2023-09-23 14:36:39 -0400

    Finished rng assignment

--- courses/artificialintelligence/assignments/rng/rng.cpp
+++ courses/artificialintelligence/assignments/rng/rng.cpp
@@ -1,4 +1,22 @@
-// add your imports here
-int main(){
-  // code here
+#include <iostream>
+#include <cstdint>
+
+int main() {
+  uint32_t random, S, N;
+  int32_t R1, R2;
+
+  std::cin >> random >> N >> R1 >> R2;
+
+  for (int i = 0; i < N; i++)
+  {
+    random = random xor (random << 13);
+    random = random xor (random >> 17);
+    random = random xor (random << 5);
+
+    uint32_t value = R1 + (random % (R2 - R1 + 1));
+
+    std::cout << value << std::endl;
+  }
+
+  return 0;
 }
